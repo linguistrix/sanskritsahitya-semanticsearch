ff4888ebe5fbd8871acdaced6f6e0dd855f4a57c
Antariksh Bothale <abothale@uw.edu>
2025-08-09 17:19:53 -0700

    Add first commit for the sanskrit sahitya semantic search demo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,57 @@
 import streamlit as st
-from openai import OpenAI
+from google import genai
+from google.genai import types
+from pinecone import Pinecone
 
-# Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+client = genai.Client(api_key=st.secrets["gemini_api_key"])
+index = pinecone.Index("sanskrit-sahitya-org")
+pinecone = Pinecone(api_key=st.secrets["pinecone_api_key"])
+
+@st.cache_data
+def query_pinecone(search_query):
+    return index.query(
+        vector=client.models.embed_content(
+            model="gemini-embedding-001",
+            contents=[search_query],
+            config=types.EmbedContentConfig(task_type="QUESTION_ANSWERING")).embeddings[0].values, 
+        top_k=5,
+        include_metadata=True,
+        include_values=False
+    )
+
+st.title("Sanskrit Sahitya Semantic Search")
+st.markdown(
+    """    
+    This note is on a free API tier and thus supports a limited number of requests. It may become temporarily unavailable if too many people use the service.    
+    
+    ---
+    This is a demo app for trying out semantic search over the SanskritSahitya.org corpus. 
+
+    You can find shlokas through free-form search queries about the content of the shloka.
+
+    Currently the following texts have been included in the Search data: Raghuvansham, Kumarasambhavam, Ramayanam, Mahabharatam, Meghadutam, Kiratarjuniyam, Rtusamharam.
+
+    You can mix and match English, Sanskrit or other languages in the query.
+    
+    **Examples** - Try entering one of these
+      - verse about an old king who is like a lamp in the morning
+      - cloud is just smoke and vapor
+      - विघ्न होते हुए भी जो नहीं डगमगाते
+"""
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
-
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
-
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
-
-        # Store and display the current prompt.
+if prompt := st.chat_input("Search query (e.g. verse about an old king being like a lamp in the morning)"):
+    if not prompt.strip():
+        st.error("Please enter some text.")
+    else:
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.markdown(prompt)
 
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+        with st.spinner("Generating response ... Please wait."):
+            response = query_pinecone(prompt)
+            if response.matches:
+                for match in response.matches:
+                    st.markdown(match.id)
+            else:
+                st.error("No match found, or API limit exhausted.")
